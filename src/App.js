b7b61eb2fdccc667ff82f5af97161b8ce76b3adb
keshav jrall <keshavjrall@outlook.com>
2023-04-29 01:14:35 +0530

    Component helper functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
+
+const Hello = (props) =>{
+
+  const bornYear =() =>{
+    const yearNow = new Date().getFullYear()
+    return yearNow-props.age
+  }
+
+  return(
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old. 
+      </p>  
+      <p>So You were probably born in the year {bornYear()}</p>
+    </div>
+  )
+}
+
+
+
+const App = () => {
+  
+  const name = "keshav"
+  const age = 21
+
+
+  return(
+    <div>
+    <h1>Greetings</h1>
+    <Hello name = {name} age = {age} />
   </div>
-)
+  )
+  }
 
 export default App
