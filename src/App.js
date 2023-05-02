06ca8806927e680b345169f487326f7cef94fb2c
keshav jrall <keshavjrall@outlook.com>
2023-05-03 00:39:38 +0530

    complex state

--- src/App.js
+++ src/App.js
@@ -1,45 +1,35 @@
-import { useState } from "react"
+import {useState} from "react"
 
-const Display =({counter})=> <div>{counter}</div>
-const Button =({handleClick,text}) => <button onClick={handleClick}>{text}</button>
-
-
-const App = () => {
-  const [counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)
-  const addByOne = () => {
-    setCounter(counter+1)
-    console.log('addByOne>>>',counter)
-  }
-  const minusByOne = () => {
-    setCounter(counter-1)
-    console.log('minusByOne>>>',counter)
-  }
-  const setToZero = () => {
-    setCounter(0)
-    console.log('setToZero>>>',counter)
-  }
-  console.log("rendering counter,....", counter)
+const App =() =>{
   
+  const [clicks,setclicks] =useState({left:0,right:0})
 
+  const handleLeftClick = () =>{
+    const newClicks ={
+      ...clicks,
+      left: clicks.left+1
+    }
+    console.log('left clicked...',clicks)
+    setclicks(newClicks)
+  }
 
-  return (
+  const handleRightClick = () =>{
+    const newClicks ={
+      ...clicks,
+      right: clicks.right+1
+    }
+    console.log('right clicked... ',clicks)
+    setclicks(newClicks)
+  }
+
+  return(
     <div>
-     <Display counter ={counter}/>
-      <Button handleClick = {addByOne}
-      text = "Add"/>
-
-
-      <Button handleClick ={minusByOne}
-        text='minus'
-      />
-
-      <Button handleClick = {setToZero}
-      text='Reset'/>
-
+        {clicks.left}
+        <button onClick={handleLeftClick}>Left</button>
+        {clicks.right}
+        <button onClick={handleRightClick}>Right</button>
     </div>
-
-
   )
 }
+
 export default App
