95bcd3ee1519340276e45b49950b3eea910a78fa
keshav jrall <keshavjrall@outlook.com>
2023-05-03 12:30:29 +0530

    Handling arrays

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@ import {useState} from "react"
 
 const App =() =>{
   
-  const [clicks,setclicks] =useState({left:0,right:0})
+  const [left,setLeft] = useState(0)
+  const [right,setRight] = useState(0)
+  const [allClicks,setAll] = useState([])
 
   const handleLeftClick = () =>{
-    const newClicks ={
-      ...clicks,
-      left: clicks.left+1
+    setAll(allClicks.concat('L'))
+    setLeft(left+1)
     }
-    console.log('left clicked...',clicks)
-    setclicks(newClicks)
-  }
-
+    
+  
   const handleRightClick = () =>{
-    const newClicks ={
-      ...clicks,
-      right: clicks.right+1
+    setAll(allClicks.concat('R'))
+    setRight(right+1)
     }
-    console.log('right clicked... ',clicks)
-    setclicks(newClicks)
-  }
+    
+  
 
   return(
     <div>
-        {clicks.left}
+        {left}
         <button onClick={handleLeftClick}>Left</button>
-        {clicks.right}
+        {right}
         <button onClick={handleRightClick}>Right</button>
+
+        <p>{allClicks.join(' ')}</p>
+
     </div>
   )
 }
