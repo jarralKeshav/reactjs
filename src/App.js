ad94f864f96747ca97f1c5ecee1f9448ce457f3f
keshav jrall <keshavjrall@outlook.com>
2023-05-15 01:43:51 +0530

    upto Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -5,29 +5,63 @@ const App =() =>{
   const [left,setLeft] = useState(0)
   const [right,setRight] = useState(0)
   const [allClicks,setAll] = useState([])
+  const [total,setTotal] = useState(0)
 
   const handleLeftClick = () =>{
     setAll(allClicks.concat('L'))
-    setLeft(left+1)
+    // setLeft(left+1)
+    const updatedLeft = left+1
+    setLeft(updatedLeft)
+    
+    setTotal(updatedLeft+right)
+    
     }
     
   
   const handleRightClick = () =>{
     setAll(allClicks.concat('R'))
     setRight(right+1)
+    setTotal(right+left)
     }
+
+    const History = (props)=> {
+      if(props.allClicks.length ===0){
+        return(
+          <div>
+            the app is used by clicking
+          </div>
+        )
+      }
+      return(
+        <div>
+          button pressed history: {props.allClicks.join(' ')}
+        </div>
+      )
+      
+    }
+
+    const Button =({handleClick,text}) =>(      
+      <button onClick={handleClick}>
+        {text}
+      </button>
+    )
+      
+    
     
   
 
   return(
     <div>
         {left}
-        <button onClick={handleLeftClick}>Left</button>
+        <Button handleClick={handleLeftClick} text ='left'/>
+        
+        <Button handleClick={handleRightClick} text='right'/>
         {right}
-        <button onClick={handleRightClick}>Right</button>
 
-        <p>{allClicks.join(' ')}</p>
 
+        {/* <p>{allClicks.join(' ')}</p> */}
+        <p>Total {total}</p>
+        <History allClicks = {allClicks}/>
     </div>
   )
 }
